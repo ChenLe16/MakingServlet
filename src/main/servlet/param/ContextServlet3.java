89885d72f989462c8b34fe0d0940ec1759cfbb73
ChenLe16 <yongchenle@gmail.com>
2021-08-08 17:41:55 +0800

    alter web.xml

--- src/main/servlet/param/ContextServlet3.java
+++ src/main/servlet/param/ContextServlet3.java
@@ -1,13 +1,19 @@
 package main.servlet.param;
 
-
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class ContextServlet2 extends HttpServlet {
+@WebServlet(
+        name = "ContextServlet3",
+        urlPatterns = "/context3",
+        initParams = {@WebInitParam(name = "authorizationToken", value = "Emily")}
+)
+public class ContextServlet3 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String token = getServletConfig().getInitParameter("authorizationToken");
