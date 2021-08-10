4664976f93742d325f4c962519eea9ad3c169edd
ChenLe16 <yongchenle@gmail.com>
2021-08-10 21:43:49 +0800

    declaration tag

--- src/main/servlet/DeclarationServlet.java
+++ src/main/servlet/DeclarationServlet.java
@@ -8,11 +8,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(
-        name = "HelloServlet",
-        urlPatterns = "/hello"
+        name = "DeclarationServlet",
+        urlPatterns = "/declare"
 )
 
-public class HelloServlet extends HttpServlet {
+public class DeclarationServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Post method");
@@ -20,17 +20,6 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().println("Get Request has been called");
-        resp.getWriter().println("Another Line!");
-
-        resp.setContentType("text/html");
-        resp.getWriter().println("<h1>header1</h1>" +
-                "<h2>header2</h2>" +
-                "<h3>header3</h3>" +
-                "<h4>header4</h4>" +
-                "<h5>header5</h5>" +
-                "<h6>header6</h6>" +
-                "<p>paragraph</p>");
+        getServletContext().getRequestDispatcher("/declaration.jsp").forward(req, resp);
     }
 }
